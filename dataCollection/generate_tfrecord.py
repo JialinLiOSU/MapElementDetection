e341e1fefe5ed1d264407553c179453a17490a38
Jialin Li <32602256+JialinLiOSU@users.noreply.github.com>
2019-07-18 16:43:14 -0400

    label map

--- dataCollection/generate_tfrecord.py
+++ dataCollection/generate_tfrecord.py
@@ -29,7 +29,7 @@ FLAGS = flags.FLAGS
 
 # TO-DO replace this with label map
 def class_text_to_int(row_label):
-    if row_label == '1':
+    if row_label == 'legend':
         return 1
     else:
         None
