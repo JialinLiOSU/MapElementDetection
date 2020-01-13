274acd70b4a69c8d52ef019ce56b03bd098677ea
Jialin Li <32602256+JialinLiOSU@users.noreply.github.com>
2020-01-13 16:56:38 -0500

    all of the choropleth map images

--- dataCollection/dataProcessingRegionMapDetection.py
+++ dataCollection/dataProcessingRegionMapDetection.py
@@ -5,7 +5,7 @@ import os
 from PIL import Image
 
 
-path = 'C:\\Users\\jiali\\Desktop\\MapElementDetection\\dataCollection\\new choropleth map\\'
+path = 'C:\\Users\\li.7957\\Desktop\\MapElementDetection\\dataCollection\\new choropleth map\\'
 
 # this size is for transfer learning (VGG16)
 width = 800
@@ -15,21 +15,19 @@ input_size = width*height*3
 input_shape = (width, height, 3)
 imageList = []
 
-newChoMapImgs = os.listdir(path)
-num_images = len(newChoMapImgs)
+mapImages = os.listdir(path)
+num_images = len(mapImages)
 
-path_target='C:\\Users\\jiali\\Desktop\\MapElementDetection\\dataCollection\\processed new choropleth map\\'
-
+path_target = 'C:\\Users\\li.7957\\Desktop\\MapElementDetection\\dataCollection\\processed choropleth map images\\'
+count = 0
 for i in range(num_images):
-    img = Image.open(path + newChoMapImgs[i])
+    img = Image.open(path + mapImages[i])
     img_resized = img.resize((width, height), Image.ANTIALIAS)
     if img not in imageList:
         imageList.append(img)
-        name_target='newChoImg'+str(i+1)+'.jpg'
+        count = count + 1
+        name_target = 'ChoImg'+str(count)+'.jpg'
         img_resized.save(path_target+name_target)
 
-print('number of images',num_images)
-print('non repeated image number',len(imageList))
-
-
-
+print('number of images', num_images)
+print('non repeated image number', len(imageList))
