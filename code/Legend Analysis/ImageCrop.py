ec1a9b5ee70c39e22077dead796b2cd835f1e5c1
JialinLiOSU <32602256+JialinLiOSU@users.noreply.github.com>
2020-04-21 21:46:22 -0400

    generate legend areas for further analysis

--- code/Legend Analysis/ImageCrop.py
+++ code/Legend Analysis/ImageCrop.py
@@ -10,8 +10,10 @@ import json
 import math
 
 dataPath = 'C:\\Users\\jiali\\Desktop\\MapElementDetection\\dataCollection\\labeledMapsWithCategory\\images'
+outputPath = 'C:\\Users\\jiali\\Desktop\\MapElementDetection\\code\\Legend Analysis\\legend images\\'
 
 def ComputeIndexRangeLegend(jsData):
+    IndexRangeList=[]
     for shape in jsData['shapes']:
         if shape['label'] == "legend":
             minX = 2000
@@ -31,22 +33,25 @@ def ComputeIndexRangeLegend(jsData):
             minY = int(minY)
             maxX = math.ceil(maxX)
             maxY = math.ceil(maxY)
-            return [minX, maxX, minY, maxY]
-        else:
-            return None
+            IndexRangeList.append([minX, maxX, minY, maxY])
+        return IndexRangeList
 
 def main():
     for filename in os.listdir(dataPath):
         if filename.endswith('.jpg'):
-            img = cv2.imread(filename)
+            img = cv2.imread(dataPath + "\\"+filename)
             jfName = filename.split(".")[0]+'.json'
             jsonFile = open(dataPath + "\\"+jfName)
             jsData = json.load(jsonFile)
-            [minX, maxX, minY, maxY] = ComputeIndexRangeLegend(jsData)
-            
-            crop_img = img[minY:maxY, minX:maxX]
-            cv2.imshow("cropped", crop_img)
-            cv2.waitKey(0)
+            indexRangeList = ComputeIndexRangeLegend(jsData)
+            if len(indexRangeList)!=0:
+                for i in range(0,len(indexRangeList)):
+                    [minX, maxX, minY, maxY] = indexRangeList[i]
+                    crop_img = img[minY:maxY, minX:maxX]
+                    outputFile = outputPath + filename.split(".")[0] +"_" + str(i) + ".jpg"
+                    cv2.imwrite(outputFile,crop_img)
+            # cv2.imshow("cropped", crop_img)
+            # cv2.waitKey(0)
 
 if __name__ == "__main__":    main()
 
