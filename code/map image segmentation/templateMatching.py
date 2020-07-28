edddc798b3f5e111728d45e70a13b84214a1dcbd
JialinLiOSU <32602256+JialinLiOSU@users.noreply.github.com>
2020-07-27 23:40:19 -0400

    add more template to test template matching

--- code/map image segmentation/templateMatching.py
+++ code/map image segmentation/templateMatching.py
@@ -2,8 +2,14 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-pathTemplate = 'C:\\Users\\jiali\\Desktop\\MapElementDetection\\code\\map image segmentation\\sample images\\template.jpg'
-pathImage = 'C:\\Users\\jiali\\Desktop\\MapElementDetection\\code\\map image segmentation\\sample images\\ChoImg45.jpg'
+pathTemplateCA = 'C:\\Users\\jiali\\Desktop\\MapElementDetection\\code\\map image segmentation\\sample images\\templateCA.jpg'
+pathTemplateCA1 = 'C:\\Users\\jiali\\Desktop\\MapElementDetection\\code\\map image segmentation\\sample images\\templateCA1.jpg'
+pathTemplateCO = 'C:\\Users\\jiali\\Desktop\\MapElementDetection\\code\\map image segmentation\\sample images\\templateCO.jpg'
+pathTemplateMI = 'C:\\Users\\jiali\\Desktop\\MapElementDetection\\code\\map image segmentation\\sample images\\templateMI.jpg'
+pathTemplateTX = 'C:\\Users\\jiali\\Desktop\\MapElementDetection\\code\\map image segmentation\\sample images\\templateTX.jpg'
+pathTemplateWI = 'C:\\Users\\jiali\\Desktop\\MapElementDetection\\code\\map image segmentation\\sample images\\templateWI.jpg'
+pathTemplateWY = 'C:\\Users\\jiali\\Desktop\\MapElementDetection\\code\\map image segmentation\\sample images\\templateWY.jpg'
+pathImage = 'C:\\Users\\jiali\\Desktop\\MapElementDetection\\code\\map image segmentation\\sample images\\ChoImg11.jpg'
 img = cv2.imread(pathImage,0)
 img2 = img.copy()
 # dataPath = 'C:\\Users\\jiali\\Desktop\\MapElementDetection\\dataCollection\\labeledMapsWithCategory\\enhImages\\'
@@ -12,8 +18,13 @@ img2 = img.copy()
 
 
 # img = cv2.imread(dataPath + imgName)
+templates = []
 
-template = cv2.imread(pathTemplate,0)
+template = cv2.imread(pathTemplateCA,0)
+templates.append(template)
+template = cv2.imread(pathTemplateCA1,0)
+templates.append(template)
+
 w, h = template.shape[::-1]
 
 # All the 6 methods for comparison in a list
@@ -23,9 +34,15 @@ methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
 for meth in methods:
     img = img2.copy()
     method = eval(meth)
+    resList = []
+    maxIndex = 0
+    for i in range(len(templates)):
 
-    # Apply template Matching
-    res = cv2.matchTemplate(img,template,method)
+        # Apply template Matching
+        res = cv2.matchTemplate(img,templates[i],method)
+        resList.append(res)
+
+    
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
 
     # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
