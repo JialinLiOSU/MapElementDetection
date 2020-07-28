c07bf1a1cd197b5050f0314a6d460a7b94beb48e
JialinLiOSU <32602256+JialinLiOSU@users.noreply.github.com>
2020-07-28 12:44:37 -0400

    easyocr test pass

---
+++ code/OCR/OCR for maps.py
@@ -0,0 +1,21 @@
+# detect texts in images
+# show an image
+import PIL
+from PIL import ImageDraw
+import os
+os.chdir(r'C:\Users\jiali\Desktop\MapElementDetection\code\OCR')
+im = PIL.Image.open("ChoImg14.jpg")
+
+import easyocr
+reader = easyocr.Reader(['en'])
+bounds = reader.readtext('ChoImg14.jpg')
+print(bounds)
+
+# def draw_boxes(image, bounds, color='yellow', width=2):
+#     draw = ImageDraw.Draw(image)
+#     for bound in bounds:
+#         p0, p1, p2, p3 = bound[0]
+#         draw.line([*p0, *p1, *p2, *p3, *p0], fill=color, width=width)
+#     return image
+
+# draw_boxes(im, bounds)
