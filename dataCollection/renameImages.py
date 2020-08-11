3d8979f1fa5e710861009e97b12bb06fe5b06df6
JialinLiOSU <32602256+JialinLiOSU@users.noreply.github.com>
2020-08-11 16:35:05 -0400

    update images

--- dataCollection/renameImages.py
+++ dataCollection/renameImages.py
@@ -2,16 +2,16 @@
 from PIL import Image
 import glob
 
-pathSource='C:\\Users\\jiali\\Desktop\\MapElementDetection\\dataCollection\\new choropleth map'
+pathSource='C:\\Users\\jiali\\Desktop\\MapElementDetection\\dataCollection\\lasted add choropleth maps'
 pathTarget='C:\\Users\\jiali\\Desktop\\MapElementDetection\\dataCollection\\preprocessedMaps\\'
 
 width=800
 height=600
-i=904
-for filename in glob.glob(pathSource+'/*.jpeg'): 
+i=944
+for filename in glob.glob(pathSource+'/*.jpg'): 
     img=Image.open(filename)
     img = img.resize((width, height), Image.ANTIALIAS)
     # if img.mode != 'RGB':
-    name_target='imageLabel'+str(i+1)+'.jpeg'
+    name_target='ChoImg'+str(i+1)+'.jpg'
     img.save(pathTarget+name_target)
     i=i+1
