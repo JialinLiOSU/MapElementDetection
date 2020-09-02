ed1c466ad5d4bfd3c8fccb1ca4ebc2ec28ab52ec
JialinLiOSU <32602256+JialinLiOSU@users.noreply.github.com>
2020-09-02 10:51:51 -0400

    change format of annotation file for self designed model

--- dataCollection/preprocessAnnotationSelfDesignedRCNN.py
+++ dataCollection/preprocessAnnotationSelfDesignedRCNN.py
@@ -6,80 +6,117 @@ import json
 import glob
 import os
 
-path = r"C:\Users\jiali\Desktop\PyTorch-YOLOv3\data\custom\labels"
-labelSourceDir = os.listdir(path)
+def centerWHtoXYrange(centerX, centerY, width, height):
+    xMin = centerX - width/2
+    xMax = centerX + width/2
+    yMin = centerY - height/2
+    yMax = centerY + height/2
+    return [xMin, xMax, yMin, yMax]
 
-pathTraValSource = r"C:\Users\jiali\Desktop\PyTorch-YOLOv3\data\custom"
+def main():
+    path = r"C:\Users\jiali\Desktop\PyTorch-YOLOv3\data\custom\labels"
+    labelSourceDir = os.listdir(path)
 
-trainFileName = pathTraValSource + '\\' + 'train.txt'
-validFileName = pathTraValSource + '\\' + 'valid.txt'
+    pathImages = r"C:\Users\jiali\Desktop\PyTorch-YOLOv3\data\custom\images"
+    imageSourceDir = os.listdir(pathImages)
+    imageFullNameList = []
+    imageShortNameList = []
+    # read image file names to a list ofr checking later
+    for imagefile in imageSourceDir: 
+        imageFullNameList.append(imagefile)
+        imageShort = imagefile.split('.')[0]
+        imageShortNameList.append(imageShort)
+    width = 800
+    height = 600
 
-# read train files to a list for checking
-trainFiles = open(trainFileName, 'r')
-trainFileList = trainFiles.read().split('\n')
-trainFiles.close()
-trainNameShortList = []
-for trainFileLong in trainFileList:
-    if trainFileLong == '' or trainFileLong == ' ':
-        continue
-    trainNameShort = trainFileLong.split('/')[3]
-    trainNameShort = trainNameShort.split('.')[0]
-    trainNameShortList.append(trainNameShort)
+    pathTraValSource = r"C:\Users\jiali\Desktop\PyTorch-YOLOv3\data\custom"
 
-# read valid files to a list for checking
-validFiles = open(validFileName, 'r')
-validFileList = validFiles.read().split('\n')
-validFiles.close()
-validNameShortList = []
-for validFileLong in validFileList:
-    if validFileLong == '' or validFileLong == ' ':
-        continue
-    validNameShort = validFileLong.split('/')[3]
-    validNameShort = validNameShort.split('.')[0]
-    validNameShortList.append(validNameShort)
+    trainFileName = pathTraValSource + '\\' + 'train.txt'
+    validFileName = pathTraValSource + '\\' + 'valid.txt'
 
-trainList = []
-testList = []
-drivePathTrain = '/content/drive/My Drive/Map element detection/cocoFormatDataTrainTest/train'
-drivePathTest = '/content/drive/My Drive/Map element detection/cocoFormatDataTrainTest/val'
+    # read train files to a list for checking
+    trainFiles = open(trainFileName, 'r')
+    trainFileList = trainFiles.read().split('\n')
+    trainFiles.close()
+    trainNameShortList = []
+    for trainFileLong in trainFileList:
+        if trainFileLong == '' or trainFileLong == ' ':
+            continue
+        trainNameShort = trainFileLong.split('/')[3]
+        trainNameShort = trainNameShort.split('.')[0]
+        trainNameShortList.append(trainNameShort)
 
-# Opening JSON file 
-for labelfile in labelSourceDir: 
-    lfShort = labelfile.split('.')[0]
-    if lfShort in trainNameShortList:
-        lfOpen = open(path + '\\' + labelfile, "r")
-        lfData = lfOpen.read().split('\n')
-        lfOpen.close()
-        for singleLable in lfData:
-            elementList = singleLable.split(' ')
-            if elementList[0] != '':
-                trainLineStr = drivePathTrain + ',' + elementList[1] + ',' + elementList[2] + ',' + \
-                    elementList[3] + ',' + elementList[4] + ',' + elementList[0] + '\n'
-                trainList.append(trainLineStr)
+    # read valid files to a list for checking
+    validFiles = open(validFileName, 'r')
+    validFileList = validFiles.read().split('\n')
+    validFiles.close()
+    validNameShortList = []
+    for validFileLong in validFileList:
+        if validFileLong == '' or validFileLong == ' ':
+            continue
+        validNameShort = validFileLong.split('/')[3]
+        validNameShort = validNameShort.split('.')[0]
+        validNameShortList.append(validNameShort)
 
-    elif lfShort in validNameShortList:
-        lfOpen = open(path + '\\' +  labelfile, "r")
-        lfData = lfOpen.read().split('\n')
-        lfOpen.close()
-        for singleLable in lfData:
-            elementList = singleLable.split(' ')
-            if elementList[0] != '':
-                testLineStr = drivePathTrain + ',' + elementList[1] + ',' + elementList[2] + ',' + \
-                    elementList[3] + ',' + elementList[4] + ',' + elementList[0] + '\n'
-                testList.append(testLineStr)
+    trainList = []
+    testList = []
+    drivePathTrain = '/content/drive/My Drive/Map element detection/cocoFormatDataTrainTest/train/'
+    drivePathTest = '/content/drive/My Drive/Map element detection/cocoFormatDataTrainTest/val/'
 
-trainAnnName = pathTraValSource + '\\' + 'trainAnnoSelfDesign.txt'
-testAnnName = pathTraValSource + '\\' + 'testAnnoSelfDesign.txt'
+    # Opening JSON file 
+    for labelfile in labelSourceDir: 
+        # find the index of this image file, and get the image file name
+        lfShort = labelfile.split('.')[0]
+        idxImage = imageShortNameList.index(lfShort)
+        imageFileName = imageFullNameList[idxImage]
 
-file = open(trainAnnName, 'a')
-file.writelines(trainList)
-file.close()
+        if lfShort in trainNameShortList:
+            lfOpen = open(path + '\\' + labelfile, "r")
+            lfData = lfOpen.read().split('\n')
+            lfOpen.close()
+            for singleLable in lfData:
+                elementList = singleLable.split(' ')
+                #elementList[12340] are centerX, centerY, labelWidth, labelHeight and labelID 
+                if elementList[0] != '':
+                    [xMin, xMax, yMin, yMax] = centerWHtoXYrange(float(elementList[1]), float(elementList[2]), float(elementList[3]), float(elementList[4]))
+                    xMin = int(xMin * width)
+                    xMax = int(xMax * width)
+                    yMin = int(yMin * height)
+                    yMax = int(yMax * height)
+                    trainLineStr = drivePathTrain + imageFileName + ',' + str(xMin) + ',' + str(yMin) + ',' + \
+                        str(xMax) + ',' + str(yMax) + ',' + elementList[0] + '\n'
+                    trainList.append(trainLineStr)
 
-file = open(testAnnName, 'a')
-file.writelines(trainList)
-file.close()
+        elif lfShort in validNameShortList:
+            lfOpen = open(path + '\\' +  labelfile, "r")
+            lfData = lfOpen.read().split('\n')
+            lfOpen.close()
+            for singleLable in lfData:
+                elementList = singleLable.split(' ')
+                if elementList[0] != '':
+                    [xMin, xMax, yMin, yMax] = centerWHtoXYrange(float(elementList[1]), float(elementList[2]), float(elementList[3]), float(elementList[4]))
+                    xMin = int(xMin * width)
+                    xMax = int(xMax * width)
+                    yMin = int(yMin * height)
+                    yMax = int(yMax * height)
+                    testLineStr = drivePathTrain + imageFileName + ',' + str(xMin) + ',' + str(yMin) + ',' + \
+                        str(xMax) + ',' + str(yMax)  + ',' + elementList[0] + '\n'
+                    testList.append(testLineStr)
+
+    trainAnnName = pathTraValSource + '\\' + 'trainAnnoSelfDesign.txt'
+    testAnnName = pathTraValSource + '\\' + 'testAnnoSelfDesign.txt'
+
+    file = open(trainAnnName, 'a')
+    file.writelines(trainList)
+    file.close()
+
+    file = open(testAnnName, 'a')
+    file.writelines(testList)
+    file.close()
     
         
+if __name__ == "__main__":
+    main()
 
 
     
